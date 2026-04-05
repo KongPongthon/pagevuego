a3e50680dfa43f0408b9b2f4bbc9f6f7b482b690
Pongsathon <k.pongsathonm@gmail.com>
2026-04-05 12:16:32 +0700

    ui

--- frontend-app/src/components/PostCard.vue
+++ frontend-app/src/components/PostCard.vue
@@ -1,4 +1,6 @@
 <script setup lang="ts">
+import { Heart, MessageCircle } from "lucide-vue-next";
+
 interface PostCardProps {
   author: string;
   authorImage: string;
@@ -14,7 +16,26 @@ const props = defineProps<PostCardProps>();
 <template>
   <div class="container">
     <div>
-      <h1>{{ props.author }}</h1>
+      <div class="container-header">
+        <div v-if="props.authorImage">
+          <img :src="props.authorImage" alt="" />
+        </div>
+        <div v-else class="card-header-img">คุณ</div>
+        <div>
+          <div>{{ props.author }}</div>
+          <div>{{ props.time }}</div>
+        </div>
+      </div>
+      <div class="container-header">
+        <div>{{ props.content }}</div>
+      </div>
+      <div class="card-body">
+        <img v-if="props.image" :src="props.image" class="card-body" />
+      </div>
+      <div class="container-header">
+        <div class="card-icon"><Heart class="icon" />{{ props.likes }}</div>
+        <div class="card-icon"><MessageCircle class="icon" />{{ props.comments }}</div>
+      </div>
     </div>
   </div>
 </template>
@@ -24,5 +45,35 @@ const props = defineProps<PostCardProps>();
   width: 100%;
   border-radius: 12px;
   box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
+  background-color: white;
+}
+.container-header {
+  display: flex;
+  gap: 8px;
+  align-items: center;
+  padding: 8px 8px;
+}
+.card-header-img {
+  width: 40px;
+  height: 40px;
+  border-width: 1px;
+  border-radius: 100%;
+  color: black;
+  background-color: #f7f9fa;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.card-body {
+  width: 100%;
+}
+.card-icon {
+  display: flex;
+  align-items: center;
+  gap: 6px;
+}
+.icon {
+  width: 16px;
+  height: 16px;
 }
 </style>
